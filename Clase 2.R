139dd1f605c91486c35c08bdc80242b670eb9b37
kacevedoh <kacevedoh@gmail.com>
2022-07-15 19:03:00 -0400

    Cree una nueva entrada

--- Clase 2.R
+++ Clase 2.R
@@ -5,3 +5,7 @@ install.packages("kableExtra")
 library(knitr)
 library(tidyverse)
 library(kableExtra)
+
+plants <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv')
+actions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv')
+threats <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv')
